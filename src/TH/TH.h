c747b9650dbc6b45f23d495bc6d133fca9c21ff1
乔靖乾 <9131062+qiao-jingqian@user.noreply.gitee.com>
2021-06-10 14:49:57 +0800

    init

---
+++ src/TH/TH.h
@@ -0,0 +1,13 @@
+#ifndef TH_INC
+#define TH_INC
+
+#include <TH/THGeneral.h>
+
+#include <TH/THBlas.h>
+#include <TH/THVector.h>
+#include <TH/THStorageFunctions.h>
+#include <TH/THTensor.h>
+#include <TH/THTensorApply.h>
+#include <TH/THTensorDimApply.h>
+
+#endif
